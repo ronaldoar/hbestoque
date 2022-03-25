5a62d97e874b017db63a612195c04063417f8541
= <ronaldo.alves.rios@gmail.com>
2022-03-25 14:53:00 -0300

    Artefatos iniciais do projeto

---
+++ src/main/java/br/com/cs/hbestoque/HbeerEstoqueApplication.java
@@ -0,0 +1,13 @@
+package br.com.cs.hbestoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbeerEstoqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbeerEstoqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cs/hbestoque/HbeerEstoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cs.hbestoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HbeerEstoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
